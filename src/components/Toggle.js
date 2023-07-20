4343cd2273802e57a4ff56e308df239e7860f1d2
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-07-20 20:27:07 +0300

    commit all changes made to toggle.js

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,39 @@
-import React from "react";
-
+import React, { useState } from "react";
+let color = "";
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState("OFF");
+  const handleClick = () => {
+    if (isOn === "OFF") {
+      setIsOn("ON");
+      color = "red";
+    } else if (isOn === "ON") {
+      setIsOn("OFF");
+      color = "green";
+    }
+  };
+  return (
+    <button onClick={handleClick} style={{ background: color }}>
+      {isOn}
+    </button>
+  );
 }
+
+/*
+function Toggle() {
+  const [isOn, setIsOn] = useState(false);
+
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+
+  const color = isOn ? "red" : "white";
+
+  return (
+    <button style={{ background: color }} onClick={handleClick}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
+}
+*/
 
 export default Toggle;
